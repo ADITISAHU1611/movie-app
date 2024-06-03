4930d9e74276442d37919d23420e57a02b61fb02
Aditi Sahu <92028980+ADITISAHU1611@users.noreply.github.com>
2024-06-03 21:38:34 +0530

    first commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const response = await fetch(`${process.env.REACT_APP_API_URL}/endpoint`);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
